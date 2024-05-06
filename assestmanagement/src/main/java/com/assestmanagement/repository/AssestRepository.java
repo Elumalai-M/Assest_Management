1119b306a7769ca8d58d573a45ed78e09c3073f7
Elumalai M <elumalai.m@laderatechnology.com>
2024-05-06 10:57:47 +0530

    dashboard_summary_query_enhanced

--- assestmanagement/src/main/java/com/assestmanagement/repository/AssestRepository.java
+++ assestmanagement/src/main/java/com/assestmanagement/repository/AssestRepository.java
@@ -15,18 +15,32 @@ public interface AssestRepository extends JpaRepository<AssetModel,Long> {
 
 	Optional<AssetModel> findByAssetId(Long assetId);
 
-	 @Query("SELECT " +
-	           "   SUM(CASE WHEN a.status = 'ASSIGNED' AND a.fixedAsset IS NOT NULL THEN 1 ELSE 0 END) AS assignedFixedAssets, " +
-	           "   SUM(CASE WHEN a.status != 'ASSIGNED' AND a.fixedAsset IS NOT NULL AND a.operationalStatus = 'WORKING' THEN 1 ELSE 0 END) AS unassignedFixedAssets, " +
-	           "   SUM(CASE WHEN a.operationalStatus = 'NOT_WORKING' AND a.fixedAsset IS NOT NULL THEN 1 ELSE 0 END) AS notWorkingFixedAssets, " +
-	           "   SUM(CASE WHEN a.status = 'ASSIGNED' AND a.itAsset IS NOT NULL THEN 1 ELSE 0 END) AS assignedItAssets, " +
-	           "   SUM(CASE WHEN a.status != 'ASSIGNED' AND a.itAsset IS NOT NULL AND a.operationalStatus = 'WORKING' THEN 1 ELSE 0 END) AS unassignedItAssets, " +
-	           "   SUM(CASE WHEN a.operationalStatus = 'NOT_WORKING' AND a.itAsset IS NOT NULL THEN 1 ELSE 0 END) AS notWorkingItAssets, " +
-	           "   COUNT(a.id) AS totalAssets, " +
-	           "   SUM(CASE WHEN a.status = 'ASSIGNED' THEN 1 ELSE 0 END) AS totalAssignedAssets, " +
-	           "   SUM(CASE WHEN a.status != 'ASSIGNED' AND a.operationalStatus = 'WORKING' THEN 1 ELSE 0 END) AS totalUnassignedAssets, " +
-	           "   SUM(CASE WHEN a.operationalStatus = 'NOT_WORKING' THEN 1 ELSE 0 END) AS totalNotWorkingAssets " +
-	           "FROM AssetModel a")
+//	 @Query("SELECT " +
+//	           "   SUM(CASE WHEN a.status = 'ASSIGNED' AND a.fixedAsset IS NOT NULL THEN 1 ELSE 0 END) AS assignedFixedAssets, " +
+//	           "   SUM(CASE WHEN a.status != 'ASSIGNED' AND a.fixedAsset IS NOT NULL AND a.operationalStatus = 'WORKING' THEN 1 ELSE 0 END) AS unassignedFixedAssets, " +
+//	           "   SUM(CASE WHEN a.operationalStatus = 'NOT_WORKING' AND a.fixedAsset IS NOT NULL THEN 1 ELSE 0 END) AS notWorkingFixedAssets, " +
+//	           "   SUM(CASE WHEN a.status = 'ASSIGNED' AND a.itAsset IS NOT NULL THEN 1 ELSE 0 END) AS assignedItAssets, " +
+//	           "   SUM(CASE WHEN a.status != 'ASSIGNED' AND a.itAsset IS NOT NULL AND a.operationalStatus = 'WORKING' THEN 1 ELSE 0 END) AS unassignedItAssets, " +
+//	           "   SUM(CASE WHEN a.operationalStatus = 'NOT_WORKING' AND a.itAsset IS NOT NULL THEN 1 ELSE 0 END) AS notWorkingItAssets, " +
+//	           "   COUNT(a.id) AS totalAssets, " +
+//	           "   SUM(CASE WHEN a.status = 'ASSIGNED' THEN 1 ELSE 0 END) AS totalAssignedAssets, " +
+//	           "   SUM(CASE WHEN a.status != 'ASSIGNED' AND a.operationalStatus = 'WORKING' THEN 1 ELSE 0 END) AS totalUnassignedAssets, " +
+//	           "   SUM(CASE WHEN a.operationalStatus = 'NOT_WORKING' THEN 1 ELSE 0 END) AS totalNotWorkingAssets " +
+//	           "FROM AssetModel a")
+	 @Query("""
+		        SELECT 
+		            SUM(CASE WHEN a.status = 'ASSIGNED' AND a.fixedAsset IS NOT NULL THEN 1 ELSE 0 END) AS assignedFixedAssets,
+		            SUM(CASE WHEN a.status != 'ASSIGNED' AND a.fixedAsset IS NOT NULL AND a.operationalStatus = 'WORKING' THEN 1 ELSE 0 END) AS unassignedFixedAssets,
+		            SUM(CASE WHEN a.operationalStatus = 'NOT_WORKING' AND a.fixedAsset IS NOT NULL THEN 1 ELSE 0 END) AS notWorkingFixedAssets,
+		            SUM(CASE WHEN a.status = 'ASSIGNED' AND a.itAsset IS NOT NULL THEN 1 ELSE 0 END) AS assignedItAssets,
+		            SUM(CASE WHEN a.status != 'ASSIGNED' AND a.itAsset IS NOT NULL AND a.operationalStatus = 'WORKING' THEN 1 ELSE 0 END) AS unassignedItAssets,
+		            SUM(CASE WHEN a.operationalStatus = 'NOT_WORKING' AND a.itAsset IS NOT NULL THEN 1 ELSE 0 END) AS notWorkingItAssets,
+		            COUNT(a.id) AS totalAssets,
+		            SUM(CASE WHEN a.status = 'ASSIGNED' THEN 1 ELSE 0 END) AS totalAssignedAssets,
+		            SUM(CASE WHEN a.status != 'ASSIGNED' AND a.operationalStatus = 'WORKING' THEN 1 ELSE 0 END) AS totalUnassignedAssets,
+		            SUM(CASE WHEN a.operationalStatus = 'NOT_WORKING' THEN 1 ELSE 0 END) AS totalNotWorkingAssets 
+		        FROM AssetModel a
+		    """)
 	AssetSummary getAssetSummary();
 
 }
